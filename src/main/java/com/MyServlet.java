3dc200d86b4d9bf5ebe74c0b760a7e7b0fce05d6
fawzanoachira <fawzanoachira@gmail.com>
2025-04-30 11:42:59 +0530

    servlet sample

--- src/main/java/com/MyServlet.java
+++ src/main/java/com/MyServlet.java
@@ -2,19 +2,21 @@ package com;
 
 import java.io.IOException;
 
-import jakarta.servlet.ServletException;
+import javax.servlet.ServletException;
+
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class MyServlet extends HttpServlet{
+public class MyServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		int firstNumber = Integer.parseInt(req.getParameter("first"));
 		int secondNumber = Integer.parseInt(req.getParameter("second"));
-		
+
 		int result = firstNumber + secondNumber;
-		
-		System.out.println(result);
+
+		PrintWriter writer = resp.getWriter();
+		writer.print("<h1>Sum of the two numbers: " + result + "</h1>");
 	}
 }
